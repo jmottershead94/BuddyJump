9ae51b6f89b48e126f3966ade85dcfe5d049c53b
jmottershead94 <jasonmottershead@hotmail.co.uk>
2015-11-15 16:00:55 +0000

    Adding in a selection screen to allow the player to choose their images.

---
+++ MyApplication/app/src/main/java/com/example/app/jason/ragerelease/app/GameStates/SelectionScreen.java
@@ -0,0 +1,59 @@
+// The package location of this class.
+package com.example.app.jason.ragerelease.app.GameStates;
+
+// All of the extra includes here.
+import android.app.Activity;
+import android.content.Context;
+import android.content.SharedPreferences;
+import android.graphics.Color;
+import android.os.Bundle;
+import android.util.DisplayMetrics;
+import android.view.WindowManager;
+import android.widget.Button;
+import android.widget.GridView;
+import android.widget.RelativeLayout;
+import android.widget.TextView;
+import android.widget.Toast;
+
+import com.example.app.jason.ragerelease.R;
+import com.example.app.jason.ragerelease.app.Framework.ImageAdapter;
+import com.example.app.jason.ragerelease.app.Framework.Level;
+import com.example.app.jason.ragerelease.app.Framework.NavigationButton;
+import com.example.app.jason.ragerelease.app.Framework.Resources;
+
+import org.jbox2d.common.Vec2;
+import org.jbox2d.dynamics.World;
+import org.w3c.dom.Text;
+
+/**
+ * Created by Jason Mottershead on 17/10/2015.
+ */
+
+// Game IS AN Activity, therefore inherits from it.
+public class SelectionScreen extends Activity
+{
+    // Attributes.
+
+
+    // Methods.
+    @Override
+    protected void onCreate(Bundle savedInstanceState)
+    {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_selection_screen);
+
+        // Initialising variables.
+        final Button playerSelectionButton = (Button) findViewById(R.id.playerSelectionButton);
+        final Button enemySelectionButton = (Button) findViewById(R.id.enemySelectionButton);
+        final Button mainMenuButton = (Button) findViewById(R.id.mainMenuButton);
+        final Button playGameButton = (Button) findViewById(R.id.playGameButton);
+        final NavigationButton button = new NavigationButton();
+
+        // If the main menu button has been pressed.
+        // Navigate the user back to the main menu.
+        button.isPressed(playerSelectionButton, this, PlayerSelection.class);
+        button.isPressed(enemySelectionButton, this, EnemySelection.class);
+        button.isPressed(mainMenuButton, this, MainMenu.class);
+        button.isPressed(playGameButton, this, Game.class);
+    }
+}
