512950ae14332163cde2842e3093fc3ee6c63d54
jmottershead94 <jasonmottershead@hotmail.co.uk>
2015-11-15 15:59:27 +0000

    Started adding in the basic framework I used over Summer.

--- MyApplication/app/src/main/java/com/example/app/jason/ragerelease/app/Framework/Resources.java
+++ MyApplication/app/src/main/java/com/example/app/jason/ragerelease/app/Framework/Resources.java
@@ -3,6 +3,8 @@ package com.example.app.jason.ragerelease.app.Framework;
 import android.content.Context;
 import android.widget.RelativeLayout;
 
+import org.jbox2d.dynamics.World;
+
 /**
  * Created by Win8 on 17/10/2015.
  */
@@ -13,16 +15,21 @@ public class Resources
     private RelativeLayout background = null;
     private int screenWidth = 0;
     private int screenHeight = 0;
+    private World world;
 
     // Methods.
-    public Resources(final Context gameContext, final RelativeLayout gameBackground, final int gameScreenWidth, final int gameScreenHeight)
+    public Resources(final Context gameContext, final RelativeLayout gameBackground, final int gameScreenWidth, final int gameScreenHeight, final World gameWorld)
     {
         // Setting up the local variables.
         context = gameContext;
         background = gameBackground;
         screenWidth = gameScreenWidth;
         screenHeight = gameScreenHeight;
+        world = gameWorld;
     }
+
+    // Setters.
+    public void setBackground(final RelativeLayout gameBackground) { background = gameBackground; }
 
     // Getters.
     // This function will return the game context.
@@ -36,4 +43,7 @@ public class Resources
 
     // This function will return the device screen height.
     public int getScreenHeight()            { return screenHeight; }
+
+    // This function will return the current Box2D world we are using.
+    public World getWorld() { return world; }
 }
--- MyApplication/app/src/main/java/com/example/app/jason/ragerelease/app/GameStates/Game.java
+++ MyApplication/app/src/main/java/com/example/app/jason/ragerelease/app/GameStates/Game.java
@@ -13,11 +13,16 @@ import android.widget.Button;
 import android.widget.GridView;
 import android.widget.RelativeLayout;
 import android.widget.TextView;
+import android.widget.Toast;
 
 import com.example.app.jason.ragerelease.R;
 import com.example.app.jason.ragerelease.app.Framework.ImageAdapter;
+import com.example.app.jason.ragerelease.app.Framework.Level;
 import com.example.app.jason.ragerelease.app.Framework.NavigationButton;
+import com.example.app.jason.ragerelease.app.Framework.Resources;
 
+import org.jbox2d.common.Vec2;
+import org.jbox2d.dynamics.World;
 import org.w3c.dom.Text;
 
 /**
@@ -28,12 +33,25 @@ import org.w3c.dom.Text;
 public class Game extends Activity
 {
     // Attributes.
+    // Private attributes.
+    // Standard library attributes.
+    private static final long desiredFPS = 60;                                  // The desired frame rate for the game.
     private static final String PREFS_NAME = "MyPrefsFile";                     // Where the options will be saved to, whether they are true or false.
-    private static final String TAG = "TKT";                                    // Used for debugging.
-    private boolean optionOneChecked, optionTwoChecked, optionThreeChecked;     // Used for gaining access to the options from the options activity.
-    private RelativeLayout background = null;
-    private GridView imageSelectionView = null;
-    private TextView textView = null;
+    private static int playerImageIndex = 0;                                    // What image the player currently wants to use for their character in the game.
+    private static int enemyImageIndex = 0;                                     // What image the player current wants to use for their enemy in the game.
+
+    // Android attributes.
+    private RelativeLayout background = null;                                   // Gives access to the relative layout background for the game.
+    private Button pauseButton = null;                                          // Gives access to the pause button for the player.
+
+    // JBox2D attributes.
+    private World world;                                                        // Gives access to the jbox2D physics engine.
+    private Vec2 gravity = null;                                                // Will be applied to dynamic objects.
+
+    // My framework attributes.
+    private MainThread gameThread = null;                                       // The main game thread that the game will be running mostly on.
+    private Level level = null;                                                 // Provides levels for the player to play in.
+    private Resources resources = null;                                         // Gives access to certain repeated resources (context, the game background, screen width , screen height, and the world), and narrows down parameters passed down.
 
     // Methods.
     @Override
@@ -44,80 +62,129 @@ public class Game extends Activity
 
         // Initialise the game.
         init();
-        //applyOptions();
+
+        // Setting the game thread to run.
+        gameThread.setRunning(true);
+        gameThread.start();
+    }
+
+    //////////////////////////////////////////////////////////
+    //======================================================//
+    //					    createWorld						//
+    //======================================================//
+    // This will create the jbox2D world, so that the       //
+    // jbox2D library can be applied to this project.       //
+    // This will also create gravity for any dynamic        //
+    // objects.                                             //
+    //////////////////////////////////////////////////////////
+    public void createWorld()
+    {
+        gravity = new Vec2(0.0f, -10.0f);
+        world = new World(gravity, false);
     }
 
     // An initialisation function for setting up the game.
     private void init()
     {
-        // Initialising variables.
-        final Button playerSelectionButton = (Button) findViewById(R.id.playerSelectionButton);
-        final Button enemySelectionButton = (Button) findViewById(R.id.enemySelectionButton);
-        final Button mainMenuButton = (Button) findViewById(R.id.mainMenuButton);
-        final Button playGameButton = (Button) findViewById(R.id.playGameButton);
-        final NavigationButton button = new NavigationButton();
-        background = (RelativeLayout) findViewById(R.id.gameBackground);
-
-//        // Accessing saved options.
-//        SharedPreferences gameSettings = getSharedPreferences(PREFS_NAME, MODE_PRIVATE);
-//        optionOneChecked = gameSettings.getBoolean("moptionOneCheckedStatus", false);
-//        optionTwoChecked = gameSettings.getBoolean("moptionTwoCheckedStatus", false);
-//        optionThreeChecked = gameSettings.getBoolean("moptionThreeCheckedStatus", false);
+        // Load in options here...
+        SharedPreferences gameSettings = getSharedPreferences(PREFS_NAME, MODE_PRIVATE);
+        playerImageIndex = gameSettings.getInt("mplayerImageIndex", 0);
+        enemyImageIndex = gameSettings.getInt("menemyImageIndex", 0);
 
         // Setting up the screen dimensions.
         DisplayMetrics displayMetrics = new DisplayMetrics();
         WindowManager windowManager = (WindowManager) getApplicationContext().getSystemService(Context.WINDOW_SERVICE);
         windowManager.getDefaultDisplay().getMetrics(displayMetrics);
 
-        // If the main menu button has been pressed.
-        // Navigate the user back to the main menu.
-        button.isPressed(playerSelectionButton, this, PlayerSelection.class);
-        button.isPressed(enemySelectionButton, this, EnemySelection.class);
-        button.isPressed(mainMenuButton, this, MainMenu.class);
-        //button.isPressed(playGameButton, this, Level.class);
+        // Setting up JBox2D.
+        createWorld();
+
+        // Initialising variables.
+        background = (RelativeLayout) findViewById(R.id.gameBackground);
+        pauseButton = (Button) findViewById(R.id.pauseButton);
+        level = new Level();
+        gameThread = new MainThread(this, desiredFPS);
+        resources = new Resources(getApplicationContext(), background, displayMetrics.widthPixels, displayMetrics.heightPixels, world);
+
+        // Initialising the level.
+        level.init(resources, this);
     }
 
-//    //////////////////////////////////////////////////////////
-//    //======================================================//
-//    //				    applyOptions   						//
-//    //======================================================//
-//    // This function will check to see the current state of //
-//    // the options, and provide an appropriate response.    //
-//    //////////////////////////////////////////////////////////
-//    public void applyOptions()
-//    {
-//        // Create different option responses here.
-//        // If the first option is ON.
-//        if(optionOneChecked)
-//        {
-//            // Let the player take a picture with the camera (user permissions required).
-//            // DEBUG - Set the background colour to blue.
-//            background.setBackgroundColor(Color.BLUE);
-//        }
-//        // If the first option is OFF.
-//        else
-//        {
-//            // Let the player select a sprite for the player character.
-//            //imageSelectionView = (GridView) findViewById(R.id.imageSelectionView);
-//            //imageSelectionView.setAdapter(new ImageAdapter(this));
-//        }
-//
-//        // If the second option is ON.
-//        if(optionTwoChecked)
-//        {
-//            // Create some text.
-//            TextView optionTwoText = new TextView(this);
-//            optionTwoText.setText("Option Two Enabled BIATCH.");
-//
-//            // Add the text to the screen.
-//            background.addView(optionTwoText);
-//        }
-//
-//        // If the third option is ON.
-//        if(optionThreeChecked)
-//        {
-//            // Rotate the background.
-//            background.setRotation(90.0f);
-//        }
-//    }
+    //////////////////////////////////////////////////////////
+    //======================================================//
+    //				        update   						//
+    //======================================================//
+    // This function will be called every frame.            //
+    // All update calls will be processed here.             //
+    //////////////////////////////////////////////////////////
+    public void update(float dt)
+    {
+        // Setting up the time step and iterations for jbox2D.
+        float timeStep = 1.0f / desiredFPS;
+        int velocityIterations = 6;
+        int positionIterations = 4;
+
+        // If the player has not paused the game.
+        if(!level.player.isPaused())
+        {
+            // Update the physics engine.
+            resources.getWorld().step(timeStep, velocityIterations, positionIterations);
+
+            // All other update calls here.
+            // Update the level.
+            level.update(dt);
+        }
+
+        // If the game is paused, the player should be able to unpause the game.
+        // They can still control the pause menu even if the game is paused.
+        level.player.uiControls(this, pauseButton);
+    }
+
+    //////////////////////////////////////////////////////////
+    //======================================================//
+    //				        render   						//
+    //======================================================//
+    // This function will update the game canvas when the   //
+    // screen updates, when the screen is "dirty".          //
+    // We do this on the UI thread so that there are no     //
+    // conflicts with the original thread that added the    //
+    // views in the first place.                            //
+    // If this were to run normally without running on      //
+    // the UI thread, errors would occur.                   //
+    //////////////////////////////////////////////////////////
+    public void render()
+    {
+        // Creating a new thread.
+        new Thread()
+        {
+            // When this new thread runs.
+            @Override
+            public void run()
+            {
+                // Try to...
+                try
+                {
+                    // Make sure that the new level objects are added to the background on the correct thread.
+                    runOnUiThread(new Runnable()
+                    {
+                        @Override
+                        public void run()
+                        {
+                            // Add all of the randomly generated objects whenever I want to the level.
+                            level.addToView();
+                        }
+                    });
+
+                    // Slight loading time, without this, below catch gives an error.
+                    Thread.sleep(500);
+                }
+                // Catch any expections with this thread.
+                catch (InterruptedException e)
+                {
+                    // Print a stack trace so we know where we went wrong.
+                    e.printStackTrace();
+                }
+            }
+        }.start();
+    }
 }
